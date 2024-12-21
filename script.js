83a164380172bc54fcf80d3e40b0aa262871f4b2
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-21 14:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-Your current approach of using innerHTML to add a new row will work, but it’s not the best practice. This is because innerHTML causes the browser to serialise the HTML content into a string, wipe out existing DOM within the element, and re-parse the string back into new DOM elements. This can be inefficient, especially for large tables.
-
-Let’s use the insertRow and insertCell methods instead. Here’s how you can modify your function:
-
 function insert_Row() {
-    let table = document.getElementById('sampleTable');
-    let newRow = table.insertRow(0); 
-
-    let cell1 = newRow.insertCell(0);
-    cell1.innerHTML = "New Cell1"; 
-
-    let cell2 = newRow.insertCell(1);
-    cell2.innerHTML = "New Cell2";
+    //Write your code here
+   let table=document.getElementById('sampleTable');
+  table.innerHTML = `<tr>
+    <td>New Cell1</td>
+    <td>New Cell2</td>
+  </tr>`+table.innerHTML 
+	
 }
